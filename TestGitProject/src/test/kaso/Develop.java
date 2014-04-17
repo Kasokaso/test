c4b0a858706932155541aa4b025e0bbb47a3f7d0
Kaso <Kaso@Kaso-PC>
2014-04-17 20:51:32 +0200

    Develop class

---
+++ TestGitProject/src/test/kaso/Develop.java
@@ -0,0 +1,14 @@
+package test.kaso;
+
+public class Develop {
+
+	public Develop() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
