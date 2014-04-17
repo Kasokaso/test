c05302929744beccd8df04bdc3698636d0ee45b9
Kaso <Kaso@Kaso-PC>
2014-04-17 18:29:31 +0200

    Initial commit

---
+++ TestGitProject/src/test/kaso/Test.java
@@ -0,0 +1,15 @@
+package test.kaso;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+		// fix1
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
