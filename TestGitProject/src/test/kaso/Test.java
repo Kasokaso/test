cfb14041d65e4b11ba0b37699440337e3f3aca56
Kaso <Kaso@Kaso-PC>
2014-04-17 19:11:53 +0200

    dev1

--- TestGitProject/src/test/kaso/Test.java
+++ TestGitProject/src/test/kaso/Test.java
@@ -4,12 +4,8 @@ public class Test {
 
 	public Test() {
 		// TODO Auto-generated constructor stub
-<<<<<<< Upstream, based on master
 		// master11
-=======
-		// fix1
 		// fix2
->>>>>>> b97ba72 fix2
 	}
 
 	public static void main(String[] args) {
