8f11844e1da2bebb7721997108f33e1d0bdd5885
Kaso <Kaso@Kaso-PC>
2014-04-17 20:52:27 +0200

    Master class

---
+++ TestGitProject/src/test/kaso/Master.java
@@ -0,0 +1,14 @@
+package test.kaso;
+
+public class Master {
+
+	public Master() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
